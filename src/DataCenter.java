146f9ebfe87851835102983fc825c1d9e644cb74
Bismar Trujillo <bismar20@hotmail.com>
2023-11-18 21:03:29 -0500

    Project 2 started

---
+++ src/DataCenter.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class DataCenter {
+
+    private List<Student> studentList;
+
+    private List<Courses> courseList;
+
+}
---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student extends Name{
+
+    private int id;
+    private Login login;
+    private Name fullnname;
+    private String major;
+    private List<Courses> courses;
+
+
+}
