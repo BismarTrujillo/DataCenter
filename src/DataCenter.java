1bee9489ada3e21adf4c0c0d7403d8fe70a6299b
Bismar Trujillo <bismar20@hotmail.com>
2023-11-19 20:27:10 -0500

    User class and methods created

--- src/DataCenter.java
+++ src/DataCenter.java
@@ -3,7 +3,6 @@ import java.util.List;
 public class DataCenter {
 
     private List<Student> studentList;
-
     private List<Courses> courseList;
 
 }
--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,11 @@
-public class Student extends Name{
+public class Student extends User {
 
     private int id;
-    private Login login;
-    private Name fullnname;
     private String major;
     private List<Courses> courses;
 
 
+    public Student(String firstName, String lastName, String username, String password) {
+        super(firstName, lastName, username, password);
+    }
 }
